de7e88155ec3ff216dd540b25c82bdbd5519c3b2
Lakshman Diwaakar <lakshman@Lakshmans-MacBook-Pro.local>
2016-08-09 23:52:47 +0900

    changed the access modifier to public so that LCM module can access it

--- src/practice/hcf/Hcf.java
+++ src/practice/hcf/Hcf.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Hcf {
-	
-	private static int hcf(int p, int q) {
+public class Hcf {
+
+	public static int hcf(int p, int q) {
 		if(q == 0) {
 			return p;
 		}
 		int remainder = p%q;
 		return hcf(q ,remainder);
 	}
-	
+
 	public static void main(String args[]) throws Exception{
 		//start : get user input
 		System.out.print("Enter space separated numbers to find Highest common factor \n");
 		Scanner reader = new Scanner(System.in);
 		String line = reader.nextLine();
 		reader.close();
-		
+
 		String[] values = line.split(" ");
 		List<Integer> numbers = new ArrayList<Integer>();
 		for(String i: values) {
