b39cfeb050a46f7fc6376267529c0510b6f71d27
lakshman <diwaakartg@gmail.com>
2016-08-14 09:19:23 +0530

    Stack ADT added

---
+++ src/practice/stack/Stack.java
@@ -0,0 +1,27 @@
+package practice.stack;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Stack<T> {
+	List <T> list;
+	
+	public Stack() {
+		list = new ArrayList<T>();
+	}
+	
+	public void push(T element) {
+		list.add(element);
+	}
+	
+	public T pop() {
+		if (list.size() <= 0) {
+			System.out.println("list empty");
+			return null;
+		}
+		return list.remove(list.size() - 1);
+	}
+	
+	public int size() {
+		return list.size();
+	}
+}
