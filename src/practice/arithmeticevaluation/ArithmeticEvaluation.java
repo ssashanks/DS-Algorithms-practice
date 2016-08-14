856a1e4cddff589d3c5ed1b1c130db060f35c8af
Shashank Swaminathan <ssashanks@gmail.com>
2016-08-14 22:58:13 +0900

    Added support for Unary addition and subtraction. Added support for binary modulo operator.

--- src/practice/arithmeticevaluation/ArithmeticEvaluation.java
+++ src/practice/arithmeticevaluation/ArithmeticEvaluation.java
@@ -9,17 +9,31 @@ public class ArithmeticEvaluation {
 	
 	Stack <String> operator;
 	Stack<Double> operand;
-	List<String> oprs = Arrays.asList("-","+", "*", "/", "++", "--", "%");
+	List<String> binOprs = Arrays.asList("-","+", "*", "/", "%");
+	List<String> unaryOprs = Arrays.asList("++", "--");
 
 	public ArithmeticEvaluation() {
 		operator = new Stack<String>();
 		operand = new Stack<Double>();
 	}
-//	
-//	public boolean isUnary(char c){
-//		
-//	}
-//	
+	
+	public boolean isUnary(char c, int i, String expression){
+		if(i < expression.length() 
+				&& expression.charAt(i-1) != c 
+				&& expression.charAt(i+1) == c) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isUnary(String opr) {
+		if(unaryOprs.contains(opr)) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 	private double operation (double v1, double v2, String op) {
 		if(op.equals("+")) {
 			return v1+v2;
@@ -29,6 +43,12 @@ public class ArithmeticEvaluation {
 			return v1 * v2;
 		} else if(op.equals("/")) {
 			return v1/v2;
+		} else if(op.equals("%")) {
+			return v1%v2;
+		} else if(op.equals("++")){
+			return v2+1;
+		} else if(op.equals("--")){
+			return v2-1;
 		}
 		return -1;
 	}
@@ -39,24 +59,31 @@ public class ArithmeticEvaluation {
 			if(Character.isDigit(c)) {
 				temp *= 10;
 				temp += Double.parseDouble(c+"");
-			} else if(oprs.contains(String.valueOf(c))) {
+			} else if(binOprs.contains(String.valueOf(c)) 
+					|| unaryOprs.contains(String.valueOf(c))) {
 				if(temp != 0) {
 					operand.push(temp);
 					temp = 0;
 				}
-				operator.push(String.valueOf(c));				
+				if(isUnary(c, i, expression)) {
+					operator.push(c+""+c);
+					i++;
+				} else {
+					operator.push(String.valueOf(c));
+				}	
 			} else if(c == ')') {
-				double value1;
+				double value1, value2 = 0;
 				if(temp != 0) {
 					value1 = temp;
-					temp =0;
+					temp = 0;
 				} else {
 					value1 = operand.pop();
 				}
-				
-				double value2 = operand.pop();
-				
-				double result = operation(value2, value1, operator.pop());
+				String opr = operator.pop();
+				if(!isUnary(opr)) {
+					value2 = operand.pop();
+				}				
+				double result = operation(value2, value1, opr );
 				operand.push(result);
 			}
 		}
@@ -64,9 +91,9 @@ public class ArithmeticEvaluation {
 		
 		return operand.pop();
 	}
-	
+
 //	public static void main(String args[]) throws Exception {		
 //		ArithmeticEvaluation obj = new ArithmeticEvaluation();
-//		System.out.println(obj.parseExpression("((50*10) *(4*5))"));	
+//		System.out.println(obj.parseExpression("(((++49)+1)+10)"));	
 //	}
 }
