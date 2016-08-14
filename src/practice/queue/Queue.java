ef3b71f642b8ba968f89bade439ae40c62eafa17
lakshman <diwaakartg@gmail.com>
2016-08-14 09:40:24 +0530

    Queue ADT added

---
+++ src/practice/queue/Queue.java
@@ -0,0 +1,38 @@
+package practice.queue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue<T> {
+	
+	List<T> list;
+	
+	public Queue() {
+		list = new ArrayList<T>();
+	}
+	
+	public void enqueue(T element) {
+		list.add(element);
+	}
+	
+	public T dequeue() {
+		if (list.size() == 0) {
+			System.out.println("Queue empty!!!");
+			return null;
+		}
+		return list.remove(0);
+	}
+	
+	public int size() {
+		return list.size();
+	}
+	
+	public boolean isEmpty() {
+		if (list.size() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
